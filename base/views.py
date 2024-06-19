0e591b94f46e1fe0d3b353a58f03f1fa9b48249d
stanjiang <devyuantian@gmail.com>
2024-06-18 21:35:48 -0700

    modify index

--- base/views.py
+++ base/views.py
@@ -16,7 +16,7 @@ def index(request):
     html = f'''
     <html>
         <body>
-            <h1>Hello from Vercel!</h1>
+            <h1>Hello from MapleBond!</h1>
             <p>The current time is { now }.</p>
         </body>
     </html>
